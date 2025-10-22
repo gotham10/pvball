f618feabef658e54eb0043ab6fb87366943e4c8d
gotham10 <notagainbruh1236@outlook.com>
2025-10-22 10:09:15 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,320 @@
+getgenv().targetModules = {
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("BatRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("BiomeRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("BrainrotRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("CardRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("CardStatRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("EventRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("GearRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("IntervalRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("PackRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("PlantRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("SeedRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("StockRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("TagRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("UpdateTimeRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("AbilityRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Types"):WaitForChild("MissionTypes"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Beginner_Wave"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Beginner_Wave2"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Beginner_Wave3"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Beginner_Wave4"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Beginner_Wave5"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Cheese_Wave1"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Cheese_Wave2"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Cheese_Wave3"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Cheese_Wave4"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Cheese_Wave5"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orangutini_Wave1"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orangutini_Wave2"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orangutini_Wave3"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orangutini_Wave4"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orangutini_Wave5"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orca_Wave1"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orca_Wave2"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orca_Wave3"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orca_Wave4"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Orca_Wave5"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Trala_Wave1"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Trala_Wave2"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Trala_Wave3"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Trala_Wave4"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("MissionRegistry"):WaitForChild("Waves"):WaitForChild("Trala_Wave5"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionDialogueOptionsType"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionEntityTypeRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionPlantIndexEnums"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionPlantStatusEnums"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionReplicationEnumsRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionReplicationRemoteOperationsEnumsRegistry"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionServerBrainrotStateType"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionStatusEffectEnums"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionWheelBarrowIndexEnums"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("MissionWheelBarrowStatusEnums"),
+    game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Registries"):WaitForChild("MissionRegistries"):WaitForChild("Types"):WaitForChild("StateMachines"):WaitForChild("MissionDefaultStateMachineStatusEnumType")
+}
+
+local HttpService = game:GetService("HttpService")
+
+local global_container
+do
+    local finder_code, global_container_obj = (function()
+        local globalenv = getgenv and getgenv() or _G or shared
+        local globalcontainer = globalenv.globalcontainer
+        if not globalcontainer then
+            globalcontainer = {}
+            globalenv.globalcontainer = globalcontainer
+        end
+        local genvs = { _G, shared }
+        if getgenv then
+            table.insert(genvs, getgenv())
+        end
+        local calllimit = 0
+        do
+            local function determineCalllimit()
+                calllimit = calllimit + 1
+                determineCalllimit()
+            end
+            pcall(determineCalllimit)
+        end
+        local function isEmpty(dict)
+            for _ in next, dict do
+                return
+            end
+            return true
+        end
+        local depth, printresults, hardlimit, query, antioverflow, matchedall
+        local function recurseEnv(env, envname)
+            if globalcontainer == env then
+                return
+            end
+            if antioverflow[env] then
+                return
+            end
+            antioverflow[env] = true
+            depth = depth + 1
+            for name, val in next, env do
+                if matchedall then
+                    break
+                end
+                local Type = type(val)
+                if Type == "table" then
+                    if depth < hardlimit then
+                        recurseEnv(val, name)
+                    end
+                elseif Type == "function" then
+                    name = string.lower(tostring(name))
+                    local matched
+                    for methodname, pattern in next, query do
+                        if pattern(name, envname) then
+                            globalcontainer[methodname] = val
+                            if not matched then
+                                matched = {}
+                            end
+                            table.insert(matched, methodname)
+                            if printresults then
+                                print(methodname, name)
+                            end
+                        end
+                    end
+                    if matched then
+                        for _, methodname in next, matched do
+                            query[methodname] = nil
+                        end
+                        matchedall = isEmpty(query)
+                        if matchedall then
+                            break
+                        end
+                    end
+                end
+            end
+            depth = depth - 1
+        end
+        local function finder(Query, ForceSearch, CustomCallLimit, PrintResults)
+            antioverflow = {}
+            query = {}
+            do
+                local function Find(String, Pattern)
+                    return string.find(String, Pattern, nil, true)
+                end
+                for methodname, pattern in next, Query do
+                    if not globalcontainer[methodname] or ForceSearch then
+                        if not Find(pattern, "return") then
+                            pattern = "return " .. pattern
+                        end
+                        query[methodname] = loadstring(pattern)
+                    end
+                end
+            end
+            depth = 0
+            printresults = PrintResults
+            hardlimit = CustomCallLimit or calllimit
+            recurseEnv(genvs)
+            do
+                local env = getfenv()
+                for methodname in next, Query do
+                    if not globalcontainer[methodname] then
+                        globalcontainer[methodname] = env[methodname]
+                    end
+                end
+            end
+            hardlimit = nil
+            depth = nil
+            printresults = nil
+            antioverflow = nil
+            query = nil
+        end
+        return finder, globalcontainer
+    end)()
+    global_container = global_container_obj
+    finder_code({
+        getscriptbytecode = 'string.find(...,"get",nil,true) and string.find(...,"bytecode",nil,true)',
+        hash = 'local a={...}local b=a[1]local function c(a,b)return string.find(a,b,nil,true)end;return c(b,"hash")and c(string.lower(tostring(a[2])),"crypt")'
+    }, true, 10)
+end
+local getscriptbytecode = global_container.getscriptbytecode
+local sha384
+if global_container.hash then
+    sha384 = function(data)
+        return global_container.hash(data, "sha384")
+    end
+end
+if not sha384 then
+    pcall(function()
+        local require_online = (function()
+            local RequireCache = {}
+            local function ARequire(ModuleScript)
+                local Cached = RequireCache[ModuleScript]
+                if Cached then
+                    return Cached
+                end
+                local Source = ModuleScript.Source
+                local LoadedSource = loadstring(Source)
+                local fenv = getfenv(LoadedSource)
+                fenv.script = ModuleScript
+                fenv.require = ARequire
+                local Output = LoadedSource()
+                RequireCache[ModuleScript] = Output
+                return Output
+            end
+            local function ARequireController(AssetId)
+                local ModuleScript = game:GetObjects("rbxassetid://" .. AssetId)[1]
+                return ARequire(ModuleScript)
+            end
+            return ARequireController
+        end)()
+        if require_online then
+            sha384 = require_online(4544052033).sha384
+        end
+    end)
+end
+local decompile = decompile
+local genv = getgenv()
+if not genv.scriptcache then
+    genv.scriptcache = {}
+end
+local ldeccache = genv.scriptcache
+local function construct_TimeoutHandler(timeout, func, timeout_return_value)
+    return function(...)
+        local args = { ... }
+        if not func then
+            return false, "Function is nil"
+        end
+        if timeout < 0 then
+            return pcall(func, table.unpack(args))
+        end
+        local thread = coroutine.running()
+        local timeoutThread, isCancelled
+        timeoutThread = task.delay(timeout, function()
+            isCancelled = true
+            coroutine.resume(thread, nil, timeout_return_value)
+        end)
+        task.spawn(function()
+            local success, result = pcall(func, table.unpack(args))
+            if isCancelled then
+                return
+            end
+            task.cancel(timeoutThread)
+            while coroutine.status(thread) ~= "suspended" do
+                task.wait()
+            end
+            coroutine.resume(thread, success, result)
+        end)
+        return coroutine.yield()
+    end
+end
+function getScriptSource(scriptInstance, timeout)
+    if not (decompile and getscriptbytecode and sha384) then
+        return false, "Error: Required functions are missing."
+    end
+    local decompileTimeout = timeout or 10
+    local getbytecode_h = construct_TimeoutHandler(3, getscriptbytecode)
+    local decompiler_h = construct_TimeoutHandler(decompileTimeout, decompile, "-- Decompiler timed out after " .. tostring(decompileTimeout) .. " seconds.")
+    local success, bytecode = getbytecode_h(scriptInstance)
+    local hashed_bytecode
+    local cached_source
+    if success and bytecode and bytecode ~= "" then
+        hashed_bytecode = sha384(bytecode)
+        cached_source = ldeccache[hashed_bytecode]
+    elseif success then
+        return true, "-- The script is empty."
+    else
+        return false, "-- Failed to get bytecode."
+    end
+    if cached_source then
+        return true, cached_source
+    end
+    local decompile_success, decompiled_source = decompiler_h(scriptInstance)
+    local output
+    if decompile_success and decompiled_source then
+        output = string.gsub(decompiled_source, "\0", "\\0")
+    else
+        output = "--[[ Failed to decompile. Reason: " .. tostring(decompiled_source) .. " ]]"
+    end
+    
+    local match_start, match_end = output:find("^(%-%- .-\n)(%-%- .-\n)(%-%- .-\n)(%-%- .-\n)(%-%- .-\n)%s*\n")
+    if match_start == 1 then
+        output = output:sub(match_end + 1)
+    end
+    
+    if hashed_bytecode then
+        ldeccache[hashed_bytecode] = output
+    end
+    return true, output
+end
+if not (decompile and getscriptbytecode and sha384) then
+    return
+end
+
+local allSourceCode = ""
+local targetModules = genv.targetModules
+
+if targetModules and type(targetModules) == "table" then
+    for _, targetModule in ipairs(targetModules) do
+        local pathString = "Invalid Module"
+        local scriptString = "ERROR: Item in targetModules is not a valid instance."
+
+        if targetModule and typeof(targetModule) == "Instance" then
+            pathString = targetModule:GetFullName()
+            local success, source_code = getScriptSource(targetModule)
+            if success then
+                scriptString = source_code
+            else
+                scriptString = "Failed to decompile " .. pathString .. ": " .. tostring(source_code)
+            end
+        else
+            
+        end
+
+        allSourceCode = allSourceCode .. "-- " .. pathString .. ":\n"
+        allSourceCode = allSourceCode .. scriptString .. "\n\n"
+    end
+else
+    allSourceCode = "ERROR: genv.targetModules is not a valid table.\n"
+end
+
+if allSourceCode == "" then
+    allSourceCode = "ERROR: No modules processed."
+end
+
+setclipboard(allSourceCode)
